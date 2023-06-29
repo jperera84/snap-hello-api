d0142cc96012c7440a24845a30263a23e126d1aa
Jose R Perera Morales <jpereramor@gmail.com>
2023-06-28 20:55:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
